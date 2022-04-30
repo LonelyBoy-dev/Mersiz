f3ebc8ed798a8ee017db2eb19ef81f3b3a73cbe4
Your Name <you@example.com>
2022-04-30 22:12:25 +0430

    add shetabit package

--- local/routes/web.php
+++ local/routes/web.php
@@ -480,6 +480,7 @@ Route::get('/torder', function () {
 
 Route::post('/tverify/{uniqueid}', function ($unique_id) {
 
+    dd('sdfsdf');
     $is_order = Order::where('refId', $unique_id)->first();
 
     if (!empty($is_order)) {
