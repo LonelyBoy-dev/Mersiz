a207baf8d27cfc60966e4440366ff89dbc50d395
Your Name <you@example.com>
2022-04-30 22:41:03 +0430

    add shetabit package

--- local/app/Http/Kernel.php
+++ local/app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\AuthGates::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Shetabit\Visitor\Middlewares\LogVisits::class,
