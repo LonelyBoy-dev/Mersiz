247489262d1c06658cfe2c4759fe0c429191e57d
Your Name <you@example.com>
2022-04-30 21:19:39 +0430

    add shetabit package

--- local/routes/web.php
+++ local/routes/web.php
@@ -428,7 +428,7 @@ Route::get('/torder', function () {
 
     return Payment::callbackUrl('https://mersiz.com/tverify/'.$invoice->getUuid())->purchase($invoice,
         function ($driver, $transactionId) {
-            dd($driver);
+            dd($driver->invoice);
             $order = new Order();
             $order->factor_number = $transactionId;
             $order->user_id = 1;
